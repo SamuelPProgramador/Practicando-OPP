03770c9af5a4923dd142e908972b990b2a877b73
Samuel Arias Peñalo <93236568+SamuelPProgramador@users.noreply.github.com>
2024-11-06 18:47:21 -0400

    Add files via upload

---
+++ POO.py
@@ -0,0 +1,12 @@
+'''
+2 clases Vehiculo y Animal
+varias clases hijas 
+1 interfa 
+subir a git con una rama desarrollo. '''
+
+class Vehiculo:
+    def __init__(self, color, marca, velocidad, model):
+        self.color = color
+        self.marca = marca
+        self.model = model
+        
